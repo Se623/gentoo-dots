856ab99ddff96a69fb2b5de3eaaaddfc49242f4d
Se623 <kabanov5erezha@yandex.ru>
2023-11-04 23:41:50 +0300

    Added workspaces

--- .config/eww/bar/scripts/workspaces.bash
+++ .config/eww/bar/scripts/workspaces.bash
@@ -1,45 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
+active=$(hyprctl activeworkspace -j | grep "id" | grep -io "[0-9]")
+ws="(box :class 'icon-small' :spacing 5 :valign 'center'"
 
-NUMBER_OF_WORKSPACES=6
+for i in 1 2 3 4 5 6 
+do
+    if [ "$i" == "$active" ]; then
+	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' '•')"
+    else
+	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' '○')"
+    fi
+done
 
-window_class() {
-    echo `hyprctl activewindow -j | jq --raw-output .class`
-}
+ws="${ws})"
 
-window_title() {
-    echo `hyprctl activewindow -j | jq --raw-output .title`
-}
-
-workspaces (){
-    WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-    seq 1 $NUMBER_OF_WORKSPACES | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
-}
-
-if [[ $1 == 'workspaces' ]]; then 
-    echo "{ \"workspaces\": $(workspaces), \"active\": 1, \"active_empty\": true }"
-    socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-        active=$(eww get workspaces | jq .active)
-        if [[ ${line:0:9} == 'workspace' ]]; then
-            active=${line:11:2}
-        fi
-
-        active_empty='true'
-        let "i = $active - 1"
-        if [[ $(workspaces | jq --raw-output .[$i].windows) -gt 0 ]]; then active_empty='false'; fi
-
-        eww update workspaces="{
-            \"workspaces\": $(workspaces),
-            \"active\": $active,
-            \"active_empty\": $active_empty
-        }" 
-
-        eww update dock_reveal="$active_empty"
-    done
-fi
-
-if [[ $1 == 'window' ]]; then 
-    window_class
-    socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-        window_class
-    done
-fi
+echo $ws
