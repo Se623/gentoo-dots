a5a21b670d8c031035e02b120560ffab6d40fc23
Se623 <kabanov5erezha@yandex.ru>
2023-11-06 11:34:55 +0300

    Updated workspace icons

--- .config/eww/bar/scripts/workspaces.bash
+++ .config/eww/bar/scripts/workspaces.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 focus=$(hyprctl activeworkspace -j | grep "id" | grep -io "[0-9]")
-ws="(box :class 'icon-small' :spacing 5 :valign 'center'"
+ws="(box :class 'icon-small' :height 40"
 
 for i in 1 2 3 4 5 6 
 do
     if [ "$i" == "$focus" ]; then
-	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text '\\ue932'))"
+	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :valign 'fill' :text ''))"
     else
-	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text '\\ue931'))"
+	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text ''))"
     fi
 done
 
