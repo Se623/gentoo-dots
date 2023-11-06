015f1273b6a68d55904caa5eaa01c3b283a5121e
Se623 <kabanov5erezha@yandex.ru>
2023-11-06 11:33:45 +0300

    Updated volume icons

--- .config/eww/bar/scripts/volume.bash
+++ .config/eww/bar/scripts/volume.bash
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-icons=$HOME/.config/eww/bar/icons/volume
 vol=$(pactl list sinks | grep "Volume" | grep -hEo "[[:digit:]]{1,3}%" | head -1 | grep -o "[^%]" | tr -d '\n')
 mute=$(pactl list sinks | grep "Mute" | grep -Eo "(yes|no)")
 
-if [ "$1" == "icon" ]; then
+if [ "$1" == "icon-a" ]; then
     if [ "$vol" -gt "66" ]; then
-	echo "\\ue90e"
+	echo "\\ue91c"
     elif [ "$vol" -gt "33" ]; then
-	echo "\\ue909"
-    elif [ "$vol" -gt "0" ]; then
-	echo "\\ue90f"
+	echo "\\ue904"
+    elif [ "$vol" -ge "0" ]; then
+	echo "\\ue902"
     fi
 if [ "$1" == "percent" ]; then
     echo "$vol"
 fi
-
+elif [ "$1" == "icon-b" ]; then
+    if [ "$vol" -gt "66" ]; then
+	echo "\\ue91c"
+    elif [ "$vol" -gt "33" ]; then
+	echo "\\ue905"
+    elif [ "$vol" -ge "0" ]; then
+	echo "\\ue903"
+    fi
+if [ "$1" == "percent" ]; then
+    echo "$vol"
+fi
+fi
