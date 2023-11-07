6cfdb8e68c3f48a0c6371c86de0ec8fd608d6b51
Se623 <kabanov5erezha@yandex.ru>
2023-11-07 07:21:16 +0300

    Some fixes
    * .config/eww/icons/clock/clock.svg:

--- .config/eww/bar/scripts/workspaces.bash
+++ .config/eww/bar/scripts/workspaces.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 focus=$(hyprctl activeworkspace -j | grep "id" | grep -io "[0-9]")
-ws="(box :class 'icon-small' :height 40"
+active=( $(hyprctl workspaces -j | grep "id" | grep -io "[0-9]" | tr '\r\n' ' ') )
+ws="(box :class 'icon-small' :height 35"
 
 for i in 1 2 3 4 5 6 
 do
+    for i
     if [ "$i" == "$focus" ]; then
 	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :valign 'fill' :text ''))"
+    elif [[ " ${active[*]} " =~ " ${i} "]]; then
+	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text 'T'))"
     else
 	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text ''))"
     fi
