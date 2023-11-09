e3120eb5132c79919b25dabd812693063b86d355
Se623 <kabanov5erezha@yandex.ru>
2023-11-09 22:52:15 +0300

    Increased workspace widget speed (also updated .gitignore)

--- .config/eww/bar/scripts/workspaces.bash
+++ .config/eww/bar/scripts/workspaces.bash
@@ -1,19 +1,28 @@
 #!/bin/bash
-focus=$(hyprctl activeworkspace -j | grep "id" | grep -io "[0-9]")
-active=( $(hyprctl workspaces -j | grep "id" | grep -io "[0-9]" | tr '\r\n' ' ') )
+focusCache=$(hyprctl activeworkspace -j | grep "id" | grep -io "[0-9]")
+activeCache=( $(hyprctl workspaces -j | grep "id" | grep -io "[0-9]" | tr '\r\n' ' ') )
 ws="(box :class 'icon-small' :height 35"
 
-for i in 1 2 3 4 5 6 
+while :
 do
-    if [ "$i" == "$focus" ]; then
-	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :valign 'fill' :text ''))"
-    elif [[ " ${active[*]} " =~ " ${i} " ]]; then
-	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text ''))"
-    else
-	ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text ''))"
+    focus=$(hyprctl activeworkspace -j | grep "id" | grep -io "[0-9]")
+    active=( $(hyprctl workspaces -j | grep "id" | grep -io "[0-9]" | tr '\r\n' ' ') )
+    if [ "$focusCache" -ne "$focus" -o "$activeCache" -ne "$active" ]; then
+	ws="(box :class 'icon-small' :height 35"
+	focusCache=$(hyprctl activeworkspace -j | grep "id" | grep -io "[0-9]")
+	activeCache=( $(hyprctl workspaces -j | grep "id" | grep -io "[0-9]" | tr '\r\n' ' ') )
+	for i in 1 2 3 4 5 6 
+	do
+	    if [ "$i" == "$focus" ]; then
+		ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :valign 'fill' :text ''))"
+	    elif [[ " ${active[*]} " =~ " ${i} " ]]; then
+		ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text ''))"
+	    else
+		ws="${ws} (button :onclick 'hyprctl dispatch workspace $i' (label :class 'icon-small' :text ''))"
+	    fi
+	done
+	ws="${ws})"
+	echo $ws
     fi
+    sleep .3
 done
-
-ws="${ws})"
-
-echo $ws
